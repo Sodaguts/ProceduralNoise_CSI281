5bd891707c22c8b8f5dc76ece9e8f73967434a78
celica lysik <celica.lysik@gmail.com>
2024-12-05 23:56:01 -0500

    Hello Triangle

--- project/main.cpp
+++ project/main.cpp
@@ -7,11 +7,8 @@
 #define SCREEN_HEIGHT 480
 
 void render();
-
 void init();
-
 void input();
-
 void cleanup();
 
 SDL_Window* window = nullptr;
@@ -19,6 +16,33 @@ SDL_GLContext context = nullptr;
 
 bool running = true;
 
+float vertices[] = 
+{
+    -0.5, -0.5, 0.0,
+     0.5, -0.5, 0.0,
+     0.0,  0.5, 0.0         
+};
+
+unsigned int vbo;
+unsigned int vao;
+unsigned int shaderProgram;
+
+const char* vertexShaderSource = R"(
+    #version 450
+    layout(location = 0) in vec3 vPos;
+    void main()
+    {
+        gl_Position = vec4(vPos,1.0);
+    }
+)";
+
+const char* fragmentShaderSource = R"(
+    #version 450
+    out vec4 FragColor;
+    void main(){
+        FragColor = vec4(1.0,0.0,0.1,1.0);
+    }
+)";
 //==========================
 //=        INIT            =
 //==========================
@@ -66,6 +90,59 @@ void init()
         printf("Glad was not initialized !!!");
         exit(1);
     } 
+
+    //create vbo
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    //create vao
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    glVertexAttribPointer(0,3,GL_FLOAT, GL_FALSE, sizeof(float) * 3, (const void*)0);
+    glEnableVertexAttribArray(0);
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    int success;
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        char infoLog[512];
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        printf("Failed to compile shader: %s", infoLog);
+    }
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        char infoLog[512];
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        printf("Failed to compile shader: %s", infoLog);
+    }
+
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success){
+        char infoLog[512];
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        printf("Failed to link shader program: %s", infoLog);
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 }
 
 //==========================
@@ -73,12 +150,22 @@ void init()
 //==========================
 void render()
 {
+    // Do setup / math up here
+
+    // clear screen to start
     glClearColor(0.95, 0.75, 0.87, 1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
 
     //render loop actual
+    glUseProgram(shaderProgram);
+    glBindVertexArray(vao);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     SDL_GL_SwapWindow(window);
+    //SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+    //SDL_RenderClear(renderer);
+
+    //SDL_RenderPresent(renderer); // use if SDL Renderer
 }
 
 //==========================
@@ -115,6 +202,10 @@ void cleanup()
     SDL_Quit(); 
 }
 
+//==========================
+//=        MAIN            =
+//==========================
+
 int main(int argc, char** argv){
 
     init();
@@ -124,10 +215,7 @@ int main(int argc, char** argv){
         input();
 
         render();
-        //SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
-        //SDL_RenderClear(renderer);
-
-        //SDL_RenderPresent(renderer);
+        
     }
 
     cleanup();
